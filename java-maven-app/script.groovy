ef89fc8ab1d5037eaf75c31360fe8810fda35461
KajalLad1206 <kajallad1994@gmail.com>
2025-05-13 03:36:13 +0530

    add update jenkins and groovy file

--- java-maven-app/script.groovy
+++ java-maven-app/script.groovy
@@ -10,13 +10,14 @@ def buildJar()
 def buildImage()
 {
     echo "building an image....!"
-    withCredentials([   
-        usernamePassword(credentialId:'dockerhub-credential', usernameVariable:'USER', passwordVariable:'PWD')
-    ])
-            
-    sh 'docker build -t kajallad126/java-maven-app:1.4 .'
-    sh 'echo $PWD | docker login -u $USER  --password-stdin'
-    sh 'docker push kajallad126/java-maven-app:1.4'
+    withCredentials([
+    usernamePassword(credentialsId: 'dockerhub-credential', usernameVariable: 'USER', passwordVariable: 'PWD')
+    ]) {
+        sh 'echo Logging into Docker...'
+        sh 'docker build -t kajallad126/java-maven-app:1.4 .'
+        sh 'echo $PWD | docker login -u $USER  --password-stdin'
+        sh 'docker push kajallad126/java-maven-app:1.4'
+    }
 }
 def deployApp(){
     echo "deploying an application...!"
